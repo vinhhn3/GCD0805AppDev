dda114dfa67d8339130b32e1ef0d55ec1d061c3c
Vinh Hoang <vinhhn3@fe.edu.vn>
2021-09-27 11:53:31 +0700

    Create list of Todo and return it to View

--- GCD0805AppDev/Controllers/TodosController.cs
+++ GCD0805AppDev/Controllers/TodosController.cs
@@ -1,5 +1,6 @@
 ﻿using GCD0805AppDev.Models;
 using System;
+using System.Collections.Generic;
 using System.Web.Mvc;
 
 namespace GCD0805AppDev.Controllers
@@ -9,13 +10,14 @@ namespace GCD0805AppDev.Controllers
     // GET: Todos
     public ActionResult Index()
     {
-      Todo todo = new Todo()
+      List<Todo> todos = new List<Todo>()
       {
-        Id = 1,
-        Description = "Kill Bill",
-        DueDate = new DateTime(2008, 12, 20)
+        new Todo(){Id = 1, Description = "Kill Bill", DueDate = new DateTime(2008, 12, 20)},
+        new Todo(){Id = 2, Description = "Kill Bill 2", DueDate = new DateTime(2012, 01, 22)},
+        new Todo(){Id = 3, Description = "John Wick", DueDate = new DateTime(2020, 01, 22)}
+
       };
-      return View(todo);
+      return View(todos);
     }
   }
 }
