dbb72496ede246b86650d9e4574f30fc05725d8f
Vinh Hoang <vinhhn3@fe.edu.vn>
2021-09-30 13:47:24 +0700

    Get todos from database

--- GCD0805AppDev/Controllers/TodosController.cs
+++ GCD0805AppDev/Controllers/TodosController.cs
@@ -1,21 +1,20 @@
 ﻿using GCD0805AppDev.Models;
-using System;
-using System.Collections.Generic;
+using System.Linq;
 using System.Web.Mvc;
 
 namespace GCD0805AppDev.Controllers
 {
   public class TodosController : Controller
   {
+    private ApplicationDbContext _context;
+    public TodosController()
+    {
+      _context = new ApplicationDbContext();
+    }
     // GET: Todos
     public ActionResult Index()
     {
-      List<Todo> todos = new List<Todo>()
-      {
-        new Todo(){Id = 1, Description = "Kill Bill", DueDate = new DateTime(2008, 12, 20)},
-        new Todo(){Id = 2, Description = "Kill Bill 2", DueDate = new DateTime(2012, 01, 22)},
-        new Todo(){Id = 3, Description = "John Wick", DueDate = new DateTime(2020, 01, 22)}
-      };
+      var todos = _context.Todos.ToList();
       return View(todos);
     }
   }
