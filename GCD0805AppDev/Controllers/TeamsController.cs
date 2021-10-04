5a5dca00158818113348799a2123bddace7e44b0
Vinh Hoang <vinhhn3@fe.edu.vn>
2021-10-04 11:38:58 +0700

    Exercise: Complete TeamsController

---
+++ GCD0805AppDev/Controllers/TeamsController.cs
@@ -0,0 +1,15 @@
+﻿using System.Web.Mvc;
+
+namespace GCD0805AppDev.Controllers
+{
+  public class TeamsController : Controller
+  {
+    // Exercise: To be completed
+    // Finish CRUD Team
+    // Only Manager can access TeamsController
+    public ActionResult Index()
+    {
+      return View();
+    }
+  }
+}
---
+++ GCD0805AppDev/Models/Team.cs
@@ -0,0 +1,13 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace GCD0805AppDev.Models
+{
+  public class Team
+  {
+    [Key]
+    public int Id { get; set; }
+    [Required]
+    [StringLength(255)]
+    public string Name { get; set; }
+  }
+}
