7ea4a9c97053c41e88108aeae986ddf42ea8b361
Vinh Hoang <vinhhn3@fe.edu.vn>
2021-10-04 08:22:45 +0700

    Add TodoCategoriesViewModel

---
+++ GCD0805AppDev/ViewModels/TodoCategoriesViewModel.cs
@@ -0,0 +1,11 @@
+﻿using GCD0805AppDev.Models;
+using System.Collections.Generic;
+
+namespace GCD0805AppDev.ViewModels
+{
+  public class TodoCategoriesViewModel
+  {
+    public Todo Todo { get; set; }
+    public IEnumerator<Category> Categories { get; set; }
+  }
+}
