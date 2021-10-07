189a6960708d8a00f8131f6c7bd489cefbcc4596
Vinh Hoang <vinhhn3@fe.edu.vn>
2021-10-07 13:37:03 +0700

    Add UserTeam model

---
+++ GCD0805AppDev/Models/UserTeam.cs
@@ -0,0 +1,17 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace GCD0805AppDev.Models
+{
+  public class UserTeam
+  {
+    [Key, Column(Order = 1)]
+    [ForeignKey("User")]
+    public string UserId { get; set; }
+    public ApplicationUser User { get; set; }
+    [Key, Column(Order = 2)]
+    [ForeignKey("Team")]
+    public int TeamId { get; set; }
+    public Team Team { get; set; }
+  }
+}
