b6b95f64456dcc014d34f11ca81e1052f8006002
Vinh Hoang <vinhhn3@fe.edu.vn>
2021-10-07 16:31:58 +0700

    CreateManager (POST) action

--- GCD0805AppDev/Controllers/AdminController.cs
+++ GCD0805AppDev/Controllers/AdminController.cs
@@ -1,5 +1,9 @@
 ﻿using GCD0805AppDev.Models;
 using GCD0805AppDev.Utils;
+using Microsoft.AspNet.Identity;
+using Microsoft.AspNet.Identity.Owin;
+using System.Threading.Tasks;
+using System.Web;
 using System.Web.Mvc;
 
 namespace GCD0805AppDev.Controllers
@@ -8,9 +12,26 @@ namespace GCD0805AppDev.Controllers
   public class AdminController : Controller
   {
     private ApplicationDbContext _context;
+    private ApplicationUserManager _userManager;
     public AdminController()
     {
+    }
+    public AdminController(ApplicationUserManager userManager)
+    {
       _context = new ApplicationDbContext();
+      UserManager = userManager;
+    }
+
+    public ApplicationUserManager UserManager
+    {
+      get
+      {
+        return _userManager ?? HttpContext.GetOwinContext().GetUserManager<ApplicationUserManager>();
+      }
+      private set
+      {
+        _userManager = value;
+      }
     }
     // GET: Admin
     public ActionResult Index()
@@ -18,9 +39,46 @@ namespace GCD0805AppDev.Controllers
       return View();
     }
 
+    [HttpGet]
     public ActionResult CreateManager()
     {
       return View();
+    }
+
+    [HttpPost]
+    [ValidateAntiForgeryToken]
+    public async Task<ActionResult> CreateManager(RegisterViewModel viewModel)
+    {
+      if (ModelState.IsValid)
+      {
+        var user = new ApplicationUser { UserName = viewModel.Email, Email = viewModel.Email };
+        var result = await UserManager.CreateAsync(user, viewModel.Password);
+        if (result.Succeeded)
+        {
+          await UserManager.AddToRoleAsync(user.Id, Role.Manager);
+
+
+          // For more information on how to enable account confirmation and password reset please visit https://go.microsoft.com/fwlink/?LinkID=320771
+          // Send an email with this link
+          // string code = await UserManager.GenerateEmailConfirmationTokenAsync(user.Id);
+          // var callbackUrl = Url.Action("ConfirmEmail", "Account", new { userId = user.Id, code = code }, protocol: Request.Url.Scheme);
+          // await UserManager.SendEmailAsync(user.Id, "Confirm your account", "Please confirm your account by clicking <a href=\"" + callbackUrl + "\">here</a>");
+
+          return RedirectToAction("Index", "Home");
+        }
+        AddErrors(result);
+      }
+
+      // If we got this far, something failed, redisplay form
+      return View(viewModel);
+    }
+
+    private void AddErrors(IdentityResult result)
+    {
+      foreach (var error in result.Errors)
+      {
+        ModelState.AddModelError("", error);
+      }
     }
   }
 }
