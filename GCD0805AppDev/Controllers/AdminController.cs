dc1902023ee77dba8403363c583a05dfd5a0b4ae
Vinh Hoang <vinhhn3@fe.edu.vn>
2021-10-07 16:19:26 +0700

    CreateManager (GET) action

---
+++ GCD0805AppDev/Controllers/AdminController.cs
@@ -0,0 +1,26 @@
+﻿using GCD0805AppDev.Models;
+using GCD0805AppDev.Utils;
+using System.Web.Mvc;
+
+namespace GCD0805AppDev.Controllers
+{
+  [Authorize(Roles = Role.Admin)]
+  public class AdminController : Controller
+  {
+    private ApplicationDbContext _context;
+    public AdminController()
+    {
+      _context = new ApplicationDbContext();
+    }
+    // GET: Admin
+    public ActionResult Index()
+    {
+      return View();
+    }
+
+    public ActionResult CreateManager()
+    {
+      return View();
+    }
+  }
+}
