a85982233648be6104a158a37163521bf5e7ae2d
Vinh Hoang <vinhhn3@fe.edu.vn>
2021-10-07 13:21:03 +0700

    Managers can see and create Team

--- GCD0805AppDev/Controllers/TeamsController.cs
+++ GCD0805AppDev/Controllers/TeamsController.cs
@@ -1,15 +1,49 @@
-﻿using System.Web.Mvc;
+﻿using GCD0805AppDev.Models;
+using GCD0805AppDev.Utils;
+using System.Linq;
+using System.Web.Mvc;
 
 namespace GCD0805AppDev.Controllers
 {
+  [Authorize(Roles = Role.Manager)]
   public class TeamsController : Controller
   {
-    // Exercise: To be completed
-    // Finish CRUD Team
-    // Only Manager can access TeamsController
+    private ApplicationDbContext _context;
+    public TeamsController()
+    {
+      _context = new ApplicationDbContext();
+    }
+    [HttpGet]
     public ActionResult Index()
+    {
+      var teams = _context.Teams.ToList();
+      return View(teams);
+    }
+
+    [HttpGet]
+    public ActionResult Create()
     {
       return View();
     }
+
+    [HttpPost]
+    public ActionResult Create(Team team)
+    {
+      if (!ModelState.IsValid)
+      {
+        return View(team);
+      }
+
+      var newTeam = new Team()
+      {
+        Name = team.Name
+      };
+
+      _context.Teams.Add(newTeam);
+      _context.SaveChanges();
+
+      return RedirectToAction("Index", "Teams");
+    }
+
   }
 }
